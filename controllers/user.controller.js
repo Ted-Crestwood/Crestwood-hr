d22352d1a56a6959f5d658212cb2f60f44cf4452
Ted@Crestwood <ted@crestwood.co.ke>
2024-05-29 14:36:50 +0300

    token

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,30 +5,30 @@ const jwt = require('jsonwebtoken');
 
 const createUser = async (req, res) => {
     try {
-       const {email,password,name,isAdmin} = req.body;
-       const existingEmail = await User.findOne({email});
-       if(existingEmail){
-        return res.status(400).json({message:"Email already exists"})
-       }
-       const hashPassword = await bcrypt.hash(password,10);
-       const newUser = await User.create({
-        isAdmin,
-        name,
-        email,
-        password: hashPassword
-       })
-       await newUser.save();
-       const token = jwt.sign(
-        {id:newUser._id},
-        process.env.JWTSECRET,
-        {
-            expiresIn:"2h"
+        const { email, password, name, isAdmin } = req.body;
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" })
         }
-    )
-    newUser.token = token
-    newUser.password = undefined
-    await new User({email,password,name,token}).save()
-       res.status(201).json({message:"User created successfully", user: newUser})
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            isAdmin,
+            name,
+            email,
+            password: hashPassword
+        })
+        await newUser.save();
+        const token = jwt.sign(
+            { id: newUser._id },
+            process.env.JWTSECRET,
+            {
+                expiresIn: "2h"
+            }
+        )
+        newUser.token = token
+        newUser.password = undefined
+        await new User({ email, password, name, token }).save()
+        res.status(201).json({ message: "User created successfully", user: {email,name}, token:token })
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -46,23 +46,23 @@ const signInUser = async (req, res) => {
             return res.status(401).json({ message: "Invalid email or password" });
         }
         const token = jwt.sign(
-            {id: user._id},
+            { id: user._id },
             process.env.JWTSECRET,
             {
-                expiresIn:"2h"
+                expiresIn: "2h"
             }
         );
         user.token = token;
         user.password = undefined
 
         const options = {
-            expires: new Date(Date.now()+ 3 *24*60*1000),
-            httpOnly:true
+            expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
+            httpOnly: true
         }
         // res.status(200).cookie("token", token,options).json({
         //     success:true, token
         // })
-        res.status(201).cookie("token", token,options).json({ message: "Sign in successfully" });
+        res.status(201).cookie("token", token, options).json({ message: "Sign in successfully" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -78,8 +78,8 @@ const getUser = async (req, res) => {
 }
 const getUserById = async (req, res) => {
     try {
-        const {id} = req.params; 
-        const user = await User.findById(id); 
+        const { id } = req.params;
+        const user = await User.findById(id);
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
@@ -93,4 +93,4 @@ const getUserById = async (req, res) => {
 
 
 
-module.exports = {createUser,getUser,signInUser,getUserById};
+module.exports = { createUser, getUser, signInUser, getUserById };
