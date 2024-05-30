4967ee432c5c6bd48a10b60c7ca0307dbd22c9bf
Ted@Crestwood <ted@crestwood.co.ke>
2024-05-30 07:04:26 +0300

    applications new schema

--- models/application.model.js
+++ models/application.model.js
@@ -1,193 +1,62 @@
 const mongoose = require("mongoose");
 
 const ApplicationSchema = new mongoose.Schema({
-  person: {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    surName: {
-      type: String,
-      required: true,
-    },
-    birthDate: {
-      type: Date,
-      required: true,
-    },
-    idNumber: {
-      type: Number,
-      required: true,
-    },
-    idImg: {
-      type: Buffer,
-      contentType: String
-    },
-    homeCounty: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
-    ethnicity: {
-      type: String,
-      required: true,
-    },
-    disability: {
-      type: Boolean,
-      required: true,
-    },
-    contact: {
-      email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true,
+  user: {
+    firstName: {type:String},
+    lastName:{type:String},
+    surname: {type:String},
+    fullName: {type:String},
+    email: {type:String},
+    phone: {type:String},
+    designation: {type:String},
+    organization: {type:String},
+    dateOfBirth: {type:String},
+    address: {type:String},
+    nationality: {type:String},
+    education: [
+       {
+        level:{type:String},
+        institution:{type:String},
+        specialization:{type:String},
+        startDate:{type:String},
+        endDate:{type:String},
       },
-      mobile: {
-        type: Number,
-        required: true,
-      },
-      address: {
-        type: String,
-        required: true,
+    ],
+    yearsOfExperience: {type:String ,default:null},
+    certifications: [
+      {
+        name: {type:String},
+        institution:{type:String},
+        date:{type:String},
       }
-    },
-    academic: [{
-      level: {
-        type: String,
-        required: true,
-      },
-      course: {
-        type: String,
-        required: true,
-      },
-      institution: {
-        type: String,
-        required: true,
-      },
-      graduation: {
-        type: Date,
-        required: true,
-      },
-      pdf: {
-        type: Buffer,
-        contentType: String
+    ],
+    workHistory: [
+      {
+        institution: {type:String},
+        role: {type:String},
+        description: {type:String},
+        joined: {type:String},
+        left: {type:String},
       }
-    }],
-    employment: [{
-      organisation: {
-        type: String,
-        required: true,
+    ],
+    referees: [
+      {
+        name: {type:String},
+        email: {type:String},
+        phone: {type:String},
+        designation: {type:String},
+        organization:{type:String},
       },
-      destination: {
-        type: String,
-        required: true,
-      },
-      startDate: {
-        type: Date,
-        required: true,
-      },
-      working: {
-        type: Boolean,
-        required: true,
-      },
-      endDate: {
-        type: Date,
-        required: true,
-      },
-      description: {
-        type: String,
-        required: true,
+    ],
+    memberships: [
+      {
+        institution: {type:String},
+        date: {type:String},
       }
-    }],
-    professional: [{
-      name: {
-        type: String,
-        required: true,
-      },
-      institution: {
-        type: String,
-        required: true,
-      },
-      year: {
-        type: Number,
-        required: true,
-      },
-      pdf: {
-        type: Buffer,
-        contentType: String
-      }
-    }],
-    shortCourses: [{
-      course: {
-        type: String,
-        required: true,
-      },
-      institution: {
-        type: String,
-        required: true,
-      },
-      year: {
-        type: Number,
-        required: true,
-      },
-      pdf: {
-        type: Buffer,
-        contentType: String
-      }
-    }],
-    membership: [{
-      institution: {
-        type: String,
-        required: true,
-      },
-      membership: {
-        type: String,
-        required: true,
-      },
-      birthDate: {
-        type: Date,
-        required: true,
-      },
-      pdf: {
-        type: Buffer,
-        contentType: String
-      }
-    }],
-    referees: [{
-      name: {
-        type: String,
-        required: true,
-      },
-      destination: {
-        type: String,
-        required: true,
-      },
-      employer: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
-      mobile: {
-        type: Number,
-        required: true,
-      }
-    }],
-    coverLetter: {
-      pdf: {
-        type: Buffer,
-        contentType: String
-      }
-    }
+    ],
+    cv: {type:String},
+    coverLetter: {type:String},
+    documents: {type:Array},
   },
 }, {
   timestamps: true,
