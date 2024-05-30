5c386b569765977bac686b881a19894d15313388
Ted@Crestwood <ted@crestwood.co.ke>
2024-05-30 07:40:01 +0300

    removing users when

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -72,7 +72,7 @@ const signInUser = async (req, res) => {
 
 const getUser = async (req, res) => {
     try {
-        const user = await User.find({}, { _id: 0 })
+        const user = await User.find({}, { _id: 0,password:0 })
         return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json({ message: error.message })
