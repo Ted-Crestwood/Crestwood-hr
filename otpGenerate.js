cc5d1dcd2fa862086da6d4bc080d9944537c12f1
Ted@Crestwood <ted@crestwood.co.ke>
2024-06-06 14:25:48 +0300

    otp sending when creating applications

--- otpGenerate.js
+++ otpGenerate.js
@@ -1,24 +1,74 @@
+// const otpGenerator = require('otp-generator');
+// const OTP = require('./models/otp.model');
+// const nodemailer = require('nodemailer');
+
+
+// const generateOtps = async ({email}) => {
+//     if(!email){
+//         console.log('No email address was submitted')
+//     }
+//     const otp = otpGenerator.generate(6, {
+//         digits: true,
+//         alphabets: false,
+//         upperCase: false,
+//         specialChars: false
+//     })
+//     try {
+//         const otpExists = await OTP.findOne({email:email})
+//         if(!otpExists){
+//             await OTP.create({ email, otp  });
+//         }
+//         await OTP.updateOne({email:email}, {otp:otp})
+//         const transporter = nodemailer.createTransport({
+//             host: process.env.HOST,
+//             port: 465,
+//             secure: true,
+//             auth: {
+//                 user: process.env.EMAIL,
+//                 pass: process.env.SMTP_PASSWORD
+//             }
+//         })
+//         transporter.sendMail({
+//             from: "recruit@crestwood.co.ke",
+//             to: email,
+//             subject: "OTP Verification",
+//             text: `Your OTP for verification is: ${otp}`
+//         })
+//         return otp;
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+// module.exports = {generateOtps}
 const otpGenerator = require('otp-generator');
 const OTP = require('./models/otp.model');
 const nodemailer = require('nodemailer');
 
-
-const generateOtps = async ({email}) => {
-    if(!email){
-        console.log('No email address was submitted')
+const generateOtps = async ({ email }) => {
+    if (!email) {
+        console.log('No email address was submitted');
+        return null;
     }
+
     const otp = otpGenerator.generate(6, {
         digits: true,
         alphabets: false,
         upperCase: false,
         specialChars: false
-    })
+    });
+
     try {
-        const otpExists = await OTP.findOne({email:email})
-        if(!otpExists){
-            await OTP.create({ email, otp  });
+        let otpRecord = await OTP.findOne({ email });
+
+        if (!otpRecord) {
+            // Create a new OTP record if it doesn't exist
+            otpRecord = await OTP.create({ email, otp });
+        } else {
+            // Update existing OTP record
+            otpRecord = await OTP.findOneAndUpdate({ email }, { otp }, { new: true });
         }
-        await OTP.updateOne({email:email}, {otp:otp})
+
+        // Send OTP via email
         const transporter = nodemailer.createTransport({
             host: process.env.HOST,
             port: 465,
@@ -27,16 +77,21 @@ const generateOtps = async ({email}) => {
                 user: process.env.EMAIL,
                 pass: process.env.SMTP_PASSWORD
             }
-        })
-        transporter.sendMail({
-            from: "recruit@crestwood.co.ke",
+        });
+
+        await transporter.sendMail({
+            from: 'recruit@crestwood.co.ke',
             to: email,
-            subject: "OTP Verification",
+            subject: 'OTP Verification',
             text: `Your OTP for verification is: ${otp}`
-        })
+        });
+
         return otp;
+
     } catch (error) {
-        console.error(error)
+        console.error('Error in generating OTP and sending email:', error);
+        throw error; // Throw the error to be caught by the caller
     }
-}
-module.exports = {generateOtps}
+};
+
+module.exports = {generateOtps};
