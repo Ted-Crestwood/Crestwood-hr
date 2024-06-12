0c6f919a23c734354d3168e0832693a7624a0688
Ted@Crestwood <ted@crestwood.co.ke>
2024-06-12 11:33:56 +0300

    get all subscribers

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -27,13 +27,13 @@ const createSubscription = async (req, res) => {
 }
 
 const getSubscribers = async (req, res) => {
-    const email = req.body;
+    const {email} = req.body;
     try {
         if (!email) {
             res.status(404).json({ message: 'User is not subscribed' })
         } else {
-            const subscriber = await Subscription.find({ email: email })
-            res.status(201).json({ message: 'Subscriber found', subscriber: subscriber })
+            const subscriber = await Subscription.find({ email: email },{_id:0})
+            res.status(201).json({ message: 'Subscriber found', subscriber: subscriber})
         }
     } catch (error) {
         res.status(500).json({ message: error.message })
